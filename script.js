d1a0723fa6b4f750ea843e0a43b86d6688f16db7
sana29goel <68688724+sana29goel@users.noreply.github.com>
2021-05-25 15:53:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-var preloader=document.getElementById('loading');
-function preloaderfunction(){
-    preloader.style.display='none';
+// var preloader=document.getElementById('loading');
+// function preloaderfunction(){
+//     preloader.style.display='none';
     
-}
+// }
+
+$(document).ready(function(){
+    $('#autoWidth').lightSlider({
+        autowidth:true,
+        loop:true,
+        onSliderLoad:function(){
+            $('#autoWidth').removeClass('cS-hidden')
+        }
+    })
+})
+
